c9a55524f3dcdc245a276c7669578682589ef956
muminamargo-source <mumina.margo@gmail.com>
2026-05-20 21:42:46 +0300

    Добавьте файлы проекта.

---
+++ CalculatorAppNet8/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAppNet8
+{
+    public class Calculator
+    {
+            public int Add(int a, int b)
+            {
+                return a + b;
+            }
+
+     
+    }
+}
---
+++ CalculatorAppNet8/Program.cs
@@ -0,0 +1,14 @@
+﻿using CalculatorAppNet8;
+
+namespace CalculatorAppNet8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            int result = calc.Add(2, 3);
+            Console.WriteLine($"2 + 3 = {result}");
+        }
+    }
+}
